d725dbe397cd4f25761c57fa68db549860af13fc
Deepak20singh <137925992+Deepak20singh@users.noreply.github.com>
2025-02-22 18:52:26 +0530

    [Deepak] Add. Code of saving message UC4

---
+++ demo/src/main/java/com/example/springgreetingapp/GreetingInfo.java
@@ -0,0 +1,29 @@
+package com.example.springgreetingapp;
+
+public class GreetingInfo {
+        private Long id;
+        private String message;
+
+        public GreetingInfo(Long id, String message) {
+            this.id = id;
+            this.message = message;
+        }
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
+    }
+
+
