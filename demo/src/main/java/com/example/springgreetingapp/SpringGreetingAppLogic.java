155e4f74e0d09e3494459b0445fa05afdd5454b5
Deepak20singh <137925992+Deepak20singh@users.noreply.github.com>
2025-02-22 19:01:07 +0530

    [Deepak] Add. Code of Fetching details of all messages UC6

---
+++ demo/src/main/java/com/example/springgreetingapp/SpringGreetingAppLogic.java
@@ -0,0 +1,78 @@
+package com.example.springgreetingapp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/request")
+public class SpringGreetingAppLogic {
+
+    @GetMapping("/get")
+    public User get(){
+        return new User("Hello from BridgeLabz!");
+    }
+    @PutMapping("/put")
+    public User putting(@RequestBody User user){
+        return new User("Updated message !"+user.getMessage());
+    }
+
+    @DeleteMapping("/delete")
+    public User deleting(){
+        return new User("Deleting message!");
+    }
+
+    @PostMapping("/post")
+    public User posting(@RequestBody User user) {
+        return new User("Received: " + user.getMessage());
+    }
+    //UC1 completed
+
+    private final GreetingService greetingService;
+
+
+    @GetMapping("/")
+    public String message(){
+        return greetingService.getGreeting();
+    }
+    @Autowired
+    public SpringGreetingAppLogic(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    //UC2 Completed
+
+    @GetMapping("/name")
+    public String name(@RequestParam(value = "name", defaultValue = "World")String name){
+        return "Hello "+name+"!";
+    }
+
+    //UC3 Completed
+    @PostMapping("/save")
+    public GreetingInfo saveGreeting(@RequestBody String message) {
+        return greetingService.save(message);
+    }
+    //UC4 Completed
+//for UC5
+    @GetMapping("/find/{id}")
+public GreetingInfo byid(@PathVariable Long id){
+        GreetingInfo greeting=greetingService.search(id);
+        if(greeting!=null){
+            return greeting;
+        }else {
+            return new GreetingInfo(-1l,"Result not Found");
+        }
+    }
+//UC5 completed
+
+    //for UC6
+    @GetMapping("/list")
+    public List<GreetingInfo> getAllGreetings() {
+        return greetingService.getAllGreetings();
+    }
+    //UC6 Completed
+
+  
+}
