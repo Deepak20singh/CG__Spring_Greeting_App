01b878c8e00cab7c945cdf886df335bc2e3c40c3
Deepak20singh <137925992+Deepak20singh@users.noreply.github.com>
2025-02-22 18:56:40 +0530

    [Deepak] Add. Code of Finding message by Id UC5

---
+++ demo/src/main/java/com/example/springgreetingapp/GreetingService.java
@@ -0,0 +1,34 @@
+package com.example.springgreetingapp;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class GreetingService {
+    public String getGreeting() {
+        return "Hello World!";
+    }
+
+//for UC4
+private Long idCounter = 1L;
+private final Map<Long, GreetingInfo> greetingMap = new HashMap<>();
+public GreetingInfo save(String message){
+    GreetingInfo greeting = new GreetingInfo(idCounter, message);
+    greetingMap.put(idCounter, greeting);
+    idCounter++;
+    return greeting;
+}
+//UC4 Completed
+
+    //for UC5
+    public GreetingInfo search(Long id){
+    return greetingMap.get(id);
+    }
+//UC5 Completed
+
+  
+}
